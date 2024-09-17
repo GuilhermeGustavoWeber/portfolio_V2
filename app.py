b5252b1620fb6c3ad568022ec90f9c6b2b199528
Guilherme Gustavo Weber <guilherme7weber@gmail.com>
2024-09-17 12:41:01 -0300

    Master (#1)
    * Add Bootstrap
    
    * Add Bootstrap

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
 app = Flask(__name__)
 
 @app.route('/')
 def port():
     return render_template("port.html")
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+      app.run(port=8085, host='0.0.0.0', debug=True, threaded=True)
