9f60d1a7d11d81525c4d3c4f0ef32e2a013cea90
Guilherme Weber <guilherme7weber@gmail.com>
2024-09-19 00:38:30 -0300

    update portifolio with bootstrap

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def port():
-    return render_template("port.html")
+def home():
+    return render_template("home.html")
+
 
 if __name__ == '__main__':
-      app.run(port=8085, host='0.0.0.0', debug=True, threaded=True)
+    app.run(port=8085, host='0.0.0.0', debug=True, threaded=True)
